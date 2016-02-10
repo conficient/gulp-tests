c4a043a22cf8b60ccbe7adc68defa21c3bcb819a
Howard Richards <dev@conficient.com>
2016-02-10 13:04:11 +0000

    Amended to use less

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
-    plumber = require('gulp-plumber');
+    less = require('gulp-less');
 
 var scripts_folder = 'src/**/*.js';
-var styles_folder = 'src/**/*.css';
+var styles_folder = 'src/*.less';
 var out = 'build';
 
 // scripts task
@@ -12,7 +12,6 @@ var out = 'build';
 gulp.task('scripts', function () {
     // get source files
     gulp.src(scripts_folder)
-        .pipe(plumber())
     // uglify
         .pipe(uglify())
     // save to build folder
@@ -22,13 +21,15 @@ gulp.task('scripts', function () {
 // copy css to build folder (no change)
 gulp.task('styles', function () {
     gulp.src(styles_folder)
-        .pipe(plumber())
-        .pipe(gulp.dest(out + '/css'));
+        .pipe(less())
+        .on('error', console.error.bind(console))
+        .pipe(gulp.dest(out + '/css/'));
 });
 
 // watches js files
 gulp.task('watch', function () {
     gulp.watch(scripts_folder, ['scripts']);
+    gulp.watch(styles_folder, ['styles']);
 });
 
 
